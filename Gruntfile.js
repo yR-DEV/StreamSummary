84b006290516be035a2d73dd8f2cee80353681fe
Zack Claar <claar.zack@gmail.com>
2016-02-08 16:23:02 -0700

    trying to figure out how to compile es6 server side with babel, and then going to get it working client side as well

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,36 @@
 module.exports = function(grunt) {
 
+      require('load-grunt-tasks')(grunt);
+      grunt.loadNpmTasks('grunt-contrib-concat');
+      grunt.loadNpmTasks('grunt-contrib-uglify');
+      grunt.loadNpmTasks('grunt-contrib-jshint');
+      grunt.loadNpmTasks('grunt-contrib-qunit');
+      grunt.loadNpmTasks('grunt-contrib-watch');
+      grunt.loadNpmTasks('grunt-contrib-clean');
+      grunt.loadNpmTasks('grunt-execute');
+
       //initial project configuration
       grunt.initConfig({
             pkg: grunt.file.readJSON('package.json'),
+
+            //cleaning the distribution directory
+            clean: ['dist'],
+
+            //babel to convert es6 to usable es5
+            babel: {
+                  options: {
+                        sourceMap: true,
+                        presets: ['es2015'],
+                        modules: 'common'
+                  },
+                  dist: {
+                        files: [{
+                              expand: true,
+                              cwd: 
+                        }]
+                  }
+            }
+
             //concatenates all the js files together
             concat: {
                   options: {
@@ -53,16 +81,10 @@ module.exports = function(grunt) {
             }
       });
 
-      grunt.loadNpmTasks('grunt-contrib-concat');
-      grunt.loadNpmTasks('grunt-contrib-uglify');
-      grunt.loadNpmTasks('grunt-contrib-jshint');
-      grunt.loadNpmTasks('grunt-contrib-qunit');
-      grunt.loadNpmTasks('grunt-contrib-watch');
-
       //task for testing
       grunt.registerTask('test', ['jshint', 'qunit']);
 
       //default task. runs when `grunt` is run from terminal
-      grunt.registerTask('default', ['concat']);
+      grunt.registerTask('default', [ 'concat' , 'uglify' ]);
 
 };
