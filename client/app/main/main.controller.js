5348cfa67f526b14357d85be0e427c715f138132
Zack Claar <claar.zack@gmail.com>
2016-02-08 22:43:35 -0700

    I just had the syntax wrong. The app is now pulling from the link via .get on an interval. Just have to store it backend now.

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -7,40 +7,32 @@ class MainController {
       constructor($http, $interval) {
             this.$http = $http;
             this.$interval = $interval;
+            var self = this;
+            this.stats = {};
 
             this.statDate = new Date();
             console.log(this.statDate);
-            //function updateStats() {
-            //setInterval(function() {
-                  // $http.get('https://api.twitch.tv/kraken/streams/summary').then(response => {
-                  //       this.stats = response.data;
-                  //       console.log('stats updated');
-                  // });
-            //}, 5000);
-            //}
-            //function updateStats(){
-            $http.get('https://api.twitch.tv/kraken/streams/summary').then(response => {
-                  this.stats = {};
-                  console.log(response.data);
-                  this.stats.channels = response.data.channels;
-                  this.stats.viewers = response.data.viewers;
-                  this.stats.date = this.statDate;
-                  console.log('stats updated');
 
-                  $http.post('/api/stats/logstats', this.stats).then(response => {
-                        console.log('*** POST RESPONSE *****');
-                        console.log(response);
-                  })
-            });
-            //}
+            var update = () => {
+                  $http.get('https://api.twitch.tv/kraken/streams/summary').then(response => {
+                        console.log(response.data);
+                        this.stats.channels = response.data.channels;
+                        this.stats.viewers = response.data.viewers;
+                        this.stats.date = this.statDate;
+                        console.log('stats updated');
+
+                        $http.post('/api/stats/logstats', this.stats).then(response => {
+                              console.log('*** POST RESPONSE *****');
+                              console.log(response);
+                        })
+                  });
+            }
+
+
+            $interval(update, 5000);
 
             console.log('inside constructor');
       }
-
-      // $interval(function() {
-      //       let newStats = updateStats();
-      //       console.log(newStats);
-      // }, 10000);
 
 }
 
