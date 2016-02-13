a5a466f85325395d1ab34fb30a9cc8e293ec4d07
Zack C <claar.zack@gmail.com>
2016-02-13 06:29:00 -0700

    Changed the 4 quarters of averages into two halfs, 12 items on each x axis, just have to go through and change naming conventions in order to match the halfs instead of quarters. I also need to go back to the database entry and create 2 halfs instead of 4 quarters

--- client/components/averagegraphs/averagegraphs.controller.js
+++ client/components/averagegraphs/averagegraphs.controller.js
@@ -10,56 +10,56 @@ class AverageGraphsController {
         let initialRender = 0;
         let ctx1 = document.getElementById('firstQuarterAverages').getContext('2d');
         let ctx2 = document.getElementById('secondQuarterAverages').getContext('2d');
-        let ctx3 = document.getElementById('thirdQuarterAverages').getContext('2d');
-        let ctx4 = document.getElementById('fourthQuarterAverages').getContext('2d');
+        // let ctx3 = document.getElementById('thirdQuarterAverages').getContext('2d');
+        // let ctx4 = document.getElementById('fourthQuarterAverages').getContext('2d');
 
         let getAverageStats = () => {
             $http.get('/api/stats/averagestats').then(response => {
 
                 let dataFirstQuarter = data1;
                 let dataSecondQuarter = data2;
-                let dataThirdQuarter = data3;
-                let dataFourthQuarter = data4;
+                // let dataThirdQuarter = data3;
+                // let dataFourthQuarter = data4;
 
-                setData(response, dataFirstQuarter, dataSecondQuarter, dataThirdQuarter, dataFourthQuarter);
+                setData(response, dataFirstQuarter, dataSecondQuarter);
             })
         }
 
-        let setData = (res, d1q, d2q, d3q, d4q) => {
+        let setData = (res, d1q, d2q) => {
             d1q.labels = [];
             d1q.datasets[0].data = [];
             d2q.labels = [];
             d2q.datasets[0].data = [];
-            d3q.labels = [];
-            d3q.datasets[0].data = [];
-            d4q.labels = [];
-            d4q.datasets[0].data = [];
+            // d3q.labels = [];
+            // d3q.datasets[0].data = [];
+            // d4q.labels = [];
+            // d4q.datasets[0].data = [];
 
             res.data[0].firstquarter.forEach(function(hour) {
                 d1q.labels.push(hour.hour + ":00");
                 d1q.datasets[0].data.push(hour.channels);
             })
             res.data[0].secondquarter.forEach(function(hour) {
+                d1q.labels.push(hour.hour + ":00");
+                d1q.datasets[0].data.push(hour.channels);
+            })
+            res.data[0].thirdquarter.forEach(function(hour) {
                 d2q.labels.push(hour.hour + ":00");
                 d2q.datasets[0].data.push(hour.channels);
             })
-            res.data[0].thirdquarter.forEach(function(hour) {
-                d3q.labels.push(hour.hour + ":00");
-                d3q.datasets[0].data.push(hour.channels);
-            })
             res.data[0].fourthquarter.forEach(function(hour) {
-                d4q.labels.push(hour.hour + ":00")
-                d4q.datasets[0].data.push(hour.channels);
+                d2q.labels.push(hour.hour + ":00")
+                d2q.datasets[0].data.push(hour.channels);
             })
 
-            renderGraphs(d1q, d2q, d3q, d4q);
+            renderGraphs(d1q, d2q);
         }
 
-        let renderGraphs = (d1, d2, d3, d4) => {
+        let renderGraphs = (d1, d2) => {
             var firstQuarterAverages = new Chart(ctx1).Line(d1);
             var secondQuarterAverages = new Chart(ctx2).Line(d2);
-            var thirdQuarterAverages = new Chart(ctx3).Line(d3);
-            var fourthQuarterAverages = new Chart(ctx4).Line(d4);
+            // var thirdQuarterAverages = new Chart(ctx3).Line(d3);
+            // var fourthQuarterAverages = new Chart(ctx4).Line(d4);
         }
 
         this.toggleChannelAverageGraphs = () => {
@@ -110,36 +110,36 @@ class AverageGraphsController {
                 },
             ]
         }
-        let data3 = {
-            labels: [],
-            datasets: [
-                {
-                    label: "Live Channels",
-                    fillColor: "rgba(187,119,209,0.2)",
-                    strokeColor: "rgba(187,119,209,1)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: []
-                },
-            ]
-        }
-        let data4 = {
-            labels: [],
-            datasets: [
-                {
-                    label: "Live Channels",
-                    fillColor: "rgba(187,119,209,0.2)",
-                    strokeColor: "rgba(187,119,209,1)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: []
-                },
-            ]
-        }
+        // let data3 = {
+        //     labels: [],
+        //     datasets: [
+        //         {
+        //             label: "Live Channels",
+        //             fillColor: "rgba(187,119,209,0.2)",
+        //             strokeColor: "rgba(187,119,209,1)",
+        //             pointColor: "rgba(187,119,209,0.75)",
+        //             pointColor: "rgba(187,119,209,0.75)",
+        //             pointHighlightFill: "#fff",
+        //             pointHighlightStroke: "rgba(151,187,205,1)",
+        //             data: []
+        //         },
+        //     ]
+        // }
+        // let data4 = {
+        //     labels: [],
+        //     datasets: [
+        //         {
+        //             label: "Live Channels",
+        //             fillColor: "rgba(187,119,209,0.2)",
+        //             strokeColor: "rgba(187,119,209,1)",
+        //             pointColor: "rgba(187,119,209,0.75)",
+        //             pointColor: "rgba(187,119,209,0.75)",
+        //             pointHighlightFill: "#fff",
+        //             pointHighlightStroke: "rgba(151,187,205,1)",
+        //             data: []
+        //         },
+        //     ]
+        // }
     }
 }
 
--- client/components/averageviewersgraphs/averageviewersgraphs.controller.js
+++ client/components/averageviewersgraphs/averageviewersgraphs.controller.js
@@ -10,8 +10,6 @@ class AverageViewerGraphsController {
         let initialRender = 0;
         let ctx1 = document.getElementById('firstQuarterViewerAverages').getContext('2d');
         let ctx2 = document.getElementById('secondQuarterViewerAverages').getContext('2d');
-        let ctx3 = document.getElementById('thirdQuarterViewerAverages').getContext('2d');
-        let ctx4 = document.getElementById('fourthQuarterViewerAverages').getContext('2d');
 
         let getAverageViewerStats = () => {
             $http.get('/api/stats/averageviewerstats').then(response => {
@@ -19,47 +17,40 @@ class AverageViewerGraphsController {
 
                 let dataFirstQuarter = data1;
                 let dataSecondQuarter = data2;
-                let dataThirdQuarter = data3;
-                let dataFourthQuarter = data4;
 
-                setData(response, dataFirstQuarter, dataSecondQuarter, dataThirdQuarter, dataFourthQuarter);
+                setData(response, dataFirstQuarter, dataSecondQuarter);
             });
         }
 
-        let setData = (res, d1q, d2q, d3q, d4q) => {
+        let setData = (res, d1q, d2q) => {
             d1q.labels = [];
             d1q.datasets[0].data = [];
             d2q.labels = [];
             d2q.datasets[0].data = [];
-            d3q.labels = [];
-            d3q.datasets[0].data = [];
-            d4q.labels = [];
-            d4q.datasets[0].data = [];
+
 
             res.data[0].firstquarter.forEach(function(hour, i) {
                 d1q.labels.push(hour.hour + ":00");
                 d1q.datasets[0].data.push(hour.viewers);
             });
             res.data[0].secondquarter.forEach(function(hour, i) {
+                d1q.labels.push(hour.hour + ":00");
+                d1q.datasets[0].data.push(hour.viewers);
+            });
+            res.data[0].thirdquarter.forEach(function(hour, i) {
                 d2q.labels.push(hour.hour + ":00");
                 d2q.datasets[0].data.push(hour.viewers);
             });
-            res.data[0].thirdquarter.forEach(function(hour, i) {
-                d3q.labels.push(hour.hour + ":00");
-                d3q.datasets[0].data.push(hour.viewers);
-            });
             res.data[0].fourthquarter.forEach(function(hour, i) {
-                d4q.labels.push(hour.hour + ":00");
-                d4q.datasets[0].data.push(hour.viewers);
+                d2q.labels.push(hour.hour + ":00");
+                d2q.datasets[0].data.push(hour.viewers);
             });
-            renderGraphs(d1q, d2q, d3q, d4q);
+            renderGraphs(d1q, d2q);
         }
 
-        let renderGraphs = (d1, d2, d3, d4) => {
+        let renderGraphs = (d1, d2) => {
             let firstQuarterViewerAveragesGraph = new Chart(ctx1).Line(d1);
             let secondQuarterViewerAveragesGraph = new Chart(ctx2).Line(d2);
-            let thirdQuarterViewerAveragesGraph = new Chart(ctx3).Line(d3);
-            let fourthQuarterViewerAveragesGraph = new Chart(ctx4).Line(d4);
         }
 
 
@@ -93,36 +84,6 @@ class AverageViewerGraphsController {
             ]
         }
         let data2 = {
-            labels: [],
-            datasets: [
-                {
-                    label: "Live Channels",
-                    fillColor: "rgba(187,119,209,0.2)",
-                    strokeColor: "rgba(187,119,209,1)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: []
-                },
-            ]
-        }
-        let data3 = {
-            labels: [],
-            datasets: [
-                {
-                    label: "Live Channels",
-                    fillColor: "rgba(187,119,209,0.2)",
-                    strokeColor: "rgba(187,119,209,1)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointColor: "rgba(187,119,209,0.75)",
-                    pointHighlightFill: "#fff",
-                    pointHighlightStroke: "rgba(151,187,205,1)",
-                    data: []
-                },
-            ]
-        }
-        let data4 = {
             labels: [],
             datasets: [
                 {
