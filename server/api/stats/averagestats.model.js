9502ea66f3178f339a7381ee7d95f04ab951fe47
Zack C <claar.zack@gmail.com>
2016-02-13 07:17:01 -0700

    I think I fixed the logic on the back end with the average stats collection and document, I may need to walk myself through the logic again when I get a chance

--- server/api/stats/averagestats.model.js
+++ server/api/stats/averagestats.model.js
@@ -5,10 +5,8 @@ import mongoose from 'mongoose';
 let Schema = mongoose.Schema;
 
 let AverageSchema = new mongoose.Schema({
-    firstquarter: Array,
-    secondquarter: Array,
-    thirdquarter: Array,
-    fourthquarter: Array
+    firsthalf: Array,
+    secondhalf: Array,
 });
 
 module.exports = mongoose.model('AverageStats', AverageSchema);
--- server/api/stats/averagestats.update.js
+++ server/api/stats/averagestats.update.js
@@ -12,27 +12,20 @@ import controller from './stats.controller';
 module.exports = {
     getAndUpdateAverageStats: function() {
         let day = new AverageSchema;
-        //console.log('gngngn12');
         StatsSchema.find().sort({"date": -1}).limit(24).then(function(data) {
             data.forEach(function(hour, i) {
                 if(i <= 11) {
                     day.firsthalf.push({ hour: i,
+                                         entries: data.entries + 1,
+                                         channels: hour.channels,
+                                         viewers: hour.viewers});
+                } else if (i >= 12 && i <= 23) {
+                    day.secondhalf.push({ hour: i,
+                                            entries: data.entries + 1,
                                             channels: hour.channels,
                                             viewers: hour.viewers});
-                } else if (i >= 12) {
-                    day.secondhalf.push({ hour: i,
-                                             channels: hour.channels,
-                                             viewers: hour.viewers});
-                }                             
-                // } else if (i >= 12 && i <= 17) {
-                //     day.thirdquarter.push({ hour: i,
-                //                             channels: hour.channels,
-                //                             viewers: hour.viewers});
-                // } else if(i >= 18 && i <= 23) {
-                //     day.fourthquarter.push({ hour: i,
-                //                              channels: hour.channels,
-                //                              viewers: hour.viewers})
-                // }
+                }
+
             });
 
             //THIS WAS FOR THE LOGIC, OF ENTERING A NEW INSTANCE INSIDE THE COLLECTION
@@ -42,32 +35,20 @@ module.exports = {
             let newDay = new AverageSchema;
 
             AverageSchema.find().sort({"date": -1}).limit(1).then(function(data) {
-                data[0].firstquarter.forEach(function(hour, i) {
-                    newDay.firstquarter.push({
+                data[0].firsthalf.forEach(function(hour, i) {
+                    newDay.firsthalf.push({
                         hour: i,
-                        channels: ((day.firstquarter[i].channels + hour.channels) / 2),
-                        viewers: ((day.firstquarter[i].viewers + hour.viewers) /  2)
+                        entries: day.entries,
+                        channels: ((day.firsthalf[i].channels + hour.channels) / day.entries),
+                        viewers: ((day.firsthalf[i].viewers + hour.viewers) /  day.entries)
                     });
                 })
-                data[0].secondquarter.forEach(function(hour, i) {
-                    newDay.secondquarter.push({
+                data[0].secondhalf.forEach(function(hour, i) {
+                    newDay.secondhalf.push({
                         hour: i,
-                        channels: ((day.secondquarter[i].channels + hour.channels) / 2),
-                        viewers: ((day.secondquarter[i].viewers + hour.viewers) /2 )
-                    })
-                })
-                data[0].thirdquarter.forEach(function(hour, i) {
-                    newDay.thirdquarter.push({
-                        hour: i,
-                        channels: ((day.thirdquarter[i].channels + hour.channels) / 2),
-                        viewers: ((day.thirdquarter[i].viewers + hour.viewers) / 2)
-                    })
-                })
-                data[0].fourthquarter.forEach(function(hour, i) {
-                    newDay.fourthquarter.push({
-                        hour: i,
-                        channels: ((day.fourthquarter[i].channels + hour.channels) / 2),
-                        viewers: ((day.fourthquarter[i].viewers + hour.viewers) / 2)
+                        entries: day.entries,
+                        channels: ((day.secondquarter[i].channels + hour.channels) / day.entries),
+                        viewers: ((day.secondquarter[i].viewers + hour.viewers) / day.entries)
                     })
                 })
             })
